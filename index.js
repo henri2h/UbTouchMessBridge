269e364dfa96f9605320e6c681249c789ac0508d
Henri <henri.carnot@hotmail.fr>
2020-03-27 23:38:58 +0100

    enable cross origin

--- index.js
+++ index.js
@@ -106,6 +106,13 @@ cl.connect((data) => {
     }
 });
 
+// enable cross origin
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 
 // functions :
 // check if all is well setup : use this to check if https is properly set
